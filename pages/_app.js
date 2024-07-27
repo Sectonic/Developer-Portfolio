9347d03826b4eee108b3f21c7ffb99e94bf0ef89
Sectonic <sujaldhakal06@gmail.com>
2024-07-27 10:28:08 -0500

    form height fix

--- pages/_app.js
+++ pages/_app.js
@@ -6,12 +6,13 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="theme-color" content="#020617"></meta>
         <title>Sujal Dhakal</title>
       </Head>
       <GoogleReCaptchaProvider reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_CLIENT_SITE_KEY} >
