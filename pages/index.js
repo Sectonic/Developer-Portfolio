45487b082181029af7a9f4e05b0600ea6bf7285a
Sujal Dhakal <sujal@phia.com>
2026-04-12 21:29:03 -0400

    hero change

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
           animate={{ opacity: 1, translateY: 0, transition: { duration: .25, delay: .45 } }}
           className="md:m-auto text-center max-w-[400px] md:w-[275px] cursor-default max-md:text-left max-md:mx-10"
         >
-          I'm an aspiring software engineer experienced in web, mobile, and game development studying Computer Science at Georgia Tech
+          I'm a SWE Intern at Phia and a CS student at Georgia Tech, with hands-on experience in full-stack and mobile development
         </motion.div>
         <motion.div
           initial={{ opacity: 0 }}
